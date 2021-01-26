f92d97bdce176fdda4b449111afbdeb6498eadba
ImadBaida <imadbaida@gmail.com>
2021-01-25 19:06:15 -0500

    Revert "pog update"
    This reverts commit e7d65e50640237652f8e84b6b1ceb010ad75111b.

--- controllers/Lab1/Lab1.java
+++ controllers/Lab1/Lab1.java
@@ -5,7 +5,7 @@ import simlejos.hardware.sensor.EV3UltrasonicSensor;
 import simlejos.robotics.RegulatedMotor;
 
 /**
- * Main class of the program. pogu
+ * Main class of the program. 
  */
 public class Lab1 {
   
